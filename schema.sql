5059a89b2f5c655a9aa33a503e46b2067bbad60c
0xbitnomad <paritosh@fren.fi>
2025-04-25 23:50:49 +0530

    Udpate frens package

--- schema.sql
+++ schema.sql
@@ -1,144 +1,129 @@
--- FUNCTIONS
-
 -- BLOCKS
 
-CREATE TABLE blocks
-(
-    slot UInt64,
-    parent_slot UInt64,
-    block_height UInt64,
-    blockhash String,
-    previous_blockhash String,
-    block_time DateTime,
-    insertion_time DateTime MATERIALIZED now(),
-)
-ENGINE = MergeTree
-PRIMARY KEY slot
-ORDER BY slot;
+CREATE TABLE blocks (
+    slot BIGINT NOT NULL,
+    parent_slot BIGINT NOT NULL,
+    block_height BIGINT NOT NULL,
+    blockhash VARCHAR NOT NULL,
+    previous_blockhash VARCHAR NOT NULL,
+    block_time TIMESTAMP NOT NULL,
+    insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (slot)
+);
 
 -- TRANSACTIONS
 
-CREATE TABLE transactions
-(
-    slot UInt64,
-    transaction_index UInt64,
-    signature String,
-    number_of_signers UInt8,
-    signer0 String,
-    signer1 String DEFAULT '',
-    signer2 String DEFAULT '',
-    signer3 String DEFAULT '',
-    signer4 String DEFAULT '',
-    signer5 String DEFAULT '',
-    signer6 String DEFAULT '',
-    signer7 String DEFAULT '',
-    -- signers Array(String),
-    PROJECTION projection_signature (SELECT * ORDER BY signature) -- RECOMMENDED
-)
-ENGINE = MergeTree
-PARTITION BY toInt64(slot / 1e6)
-PRIMARY KEY (slot, transaction_index)
-ORDER BY (slot, transaction_index);
+CREATE TABLE transactions (
+    slot BIGINT NOT NULL,
+    transaction_index BIGINT NOT NULL,
+    signature VARCHAR NOT NULL,
+    number_of_signers SMALLINT NOT NULL,
+    signer0 VARCHAR NOT NULL,
+    signer1 VARCHAR DEFAULT '',
+    signer2 VARCHAR DEFAULT '',
+    signer3 VARCHAR DEFAULT '',
+    signer4 VARCHAR DEFAULT '',
+    signer5 VARCHAR DEFAULT '',
+    signer6 VARCHAR DEFAULT '',
+    signer7 VARCHAR DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (slot, transaction_index)
+);
+
+CREATE INDEX idx_transactions_signature ON transactions (signature);
 
 -- FRENS EVENTS
 
-CREATE TABLE frens_pool_create_events
-(
-    slot UInt64,
-    transaction_index UInt64,
-    instruction_index UInt64,
-    partial_signature String,
-    partial_blockhash String,
-    trx_hash String,
-    platform_id String,
-    pool_state String,
-    creator String,   
-    config String,
-    curve_name String,
-    symbol String,
-    uri String,
-    decimals UInt8,         
-    curve_param String,
-    total_locked_amount UInt64 DEFAULT 0,
-    cliff_period UInt64 DEFAULT 0,
-    unlock_period UInt64 DEFAULT 0,
-    parent_instruction_index Int64 DEFAULT -1,
-    top_instruction_index Int64 DEFAULT -1,
-    parent_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-    top_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-)
-ENGINE = MergeTree
-PRIMARY KEY (slot, transaction_index, instruction_index)
-ORDER BY (slot, transaction_index, instruction_index);
+CREATE TABLE frens_pool_create_events (
+    slot BIGINT NOT NULL,
+    transaction_index BIGINT NOT NULL,
+    instruction_index BIGINT NOT NULL,
+    partial_signature VARCHAR NOT NULL,
+    partial_blockhash VARCHAR NOT NULL,
+    trx_hash VARCHAR NOT NULL,
+    platform_id VARCHAR NOT NULL,
+    pool_state VARCHAR NOT NULL,
+    creator VARCHAR NOT NULL,
+    config VARCHAR NOT NULL,
+    curve_name VARCHAR NOT NULL,
+    symbol VARCHAR NOT NULL,
+    uri VARCHAR NOT NULL,
+    decimals SMALLINT NOT NULL,
+    curve_param VARCHAR NOT NULL,
+    total_locked_amount BIGINT DEFAULT 0,
+    cliff_period BIGINT DEFAULT 0,
+    unlock_period BIGINT DEFAULT 0,
+    parent_instruction_index BIGINT DEFAULT -1,
+    top_instruction_index BIGINT DEFAULT -1,
+    parent_instruction_program_id VARCHAR DEFAULT '',
+    top_instruction_program_id VARCHAR DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (slot, transaction_index, instruction_index)
+);
 
-CREATE TABLE frens_trade_events
-(
-    slot UInt64,
-    transaction_index UInt64,
-    instruction_index UInt64,
-    partial_signature String,
-    partial_blockhash String,
-    trx_hash String,
-    platform_id String,
-    total_base_sell UInt64 DEFAULT 0,
-    virtual_base UInt64 DEFAULT 0,
-    virtual_quote UInt64 DEFAULT 0,
-    real_base_before UInt64 DEFAULT 0,
-    real_quote_before UInt64 DEFAULT 0,
-    real_base_after UInt64 DEFAULT 0,
-    real_quote_after UInt64 DEFAULT 0,
-    amount_in UInt64 DEFAULT 0,
-    amount_out UInt64 DEFAULT 0,
-    protocol_fee UInt64 DEFAULT 0,
-    platform_fee UInt64 DEFAULT 0,
-    share_fee UInt64 DEFAULT 0,
-    trade_direction String,
-    pool_status String,
-    parent_instruction_index Int64 DEFAULT -1,
-    top_instruction_index Int64 DEFAULT -1,
-    parent_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-    top_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-)
-ENGINE = MergeTree
-PRIMARY KEY (slot, transaction_index, instruction_index)
-ORDER BY (slot, transaction_index, instruction_index);
+CREATE TABLE frens_trade_events (
+    slot BIGINT NOT NULL,
+    transaction_index BIGINT NOT NULL,
+    instruction_index BIGINT NOT NULL,
+    partial_signature VARCHAR NOT NULL,
+    partial_blockhash VARCHAR NOT NULL,
+    trx_hash VARCHAR NOT NULL,
+    platform_id VARCHAR NOT NULL,
+    total_base_sell BIGINT DEFAULT 0,
+    virtual_base BIGINT DEFAULT 0,
+    virtual_quote BIGINT DEFAULT 0,
+    real_base_before BIGINT DEFAULT 0,
+    real_quote_before BIGINT DEFAULT 0,
+    real_base_after BIGINT DEFAULT 0,
+    real_quote_after BIGINT DEFAULT 0,
+    amount_in BIGINT DEFAULT 0,
+    amount_out BIGINT DEFAULT 0,
+    protocol_fee BIGINT DEFAULT 0,
+    platform_fee BIGINT DEFAULT 0,
+    share_fee BIGINT DEFAULT 0,
+    trade_direction VARCHAR NOT NULL,
+    pool_status VARCHAR NOT NULL,
+    parent_instruction_index BIGINT DEFAULT -1,
+    top_instruction_index BIGINT DEFAULT -1,
+    parent_instruction_program_id VARCHAR DEFAULT '',
+    top_instruction_program_id VARCHAR DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (slot, transaction_index, instruction_index)
+);
 
-CREATE TABLE frens_create_vesting_events
-(
-    slot UInt64,
-    transaction_index UInt64,
-    instruction_index UInt64,
-    partial_signature String,
-    partial_blockhash String,
-    trx_hash String,
-    pool_state String,
-    beneficiary String,
-    share_amount UInt64 DEFAULT 0,
-    parent_instruction_index Int64 DEFAULT -1,
-    top_instruction_index Int64 DEFAULT -1,
-    parent_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-    top_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-)
-ENGINE = MergeTree
-PRIMARY KEY (slot, transaction_index, instruction_index)
-ORDER BY (slot, transaction_index, instruction_index);
+CREATE TABLE frens_create_vesting_events (
+    slot BIGINT NOT NULL,
+    transaction_index BIGINT NOT NULL,
+    instruction_index BIGINT NOT NULL,
+    partial_signature VARCHAR NOT NULL,
+    partial_blockhash VARCHAR NOT NULL,
+    trx_hash VARCHAR NOT NULL,
+    pool_state VARCHAR NOT NULL,
+    beneficiary VARCHAR NOT NULL,
+    share_amount BIGINT DEFAULT 0,
+    parent_instruction_index BIGINT DEFAULT -1,
+    top_instruction_index BIGINT DEFAULT -1,
+    parent_instruction_program_id VARCHAR DEFAULT '',
+    top_instruction_program_id VARCHAR DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (slot, transaction_index, instruction_index)
+);
 
-CREATE TABLE frens_claim_vested_events
-(
-    slot UInt64,
-    transaction_index UInt64,
-    instruction_index UInt64,
-    partial_signature String,
-    partial_blockhash String,
-    trx_hash String,
-    pool_state String,
-    beneficiary String,
-    claim_amount UInt64 DEFAULT 0,
-    parent_instruction_index Int64 DEFAULT -1,
-    top_instruction_index Int64 DEFAULT -1,
-    parent_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-    top_instruction_program_id LowCardinality(String) DEFAULT '' CODEC(LZ4),
-)
-ENGINE = MergeTree
-PRIMARY KEY (slot, transaction_index, instruction_index)
-ORDER BY (slot, transaction_index, instruction_index);
+CREATE TABLE frens_claim_vested_events (
+    slot BIGINT NOT NULL,
+    transaction_index BIGINT NOT NULL,
+    instruction_index BIGINT NOT NULL,
+    partial_signature VARCHAR NOT NULL,
+    partial_blockhash VARCHAR NOT NULL,
+    trx_hash VARCHAR NOT NULL,
+    pool_state VARCHAR NOT NULL,
+    beneficiary VARCHAR NOT NULL,
+    claim_amount BIGINT DEFAULT 0,
+    parent_instruction_index BIGINT DEFAULT -1,
+    top_instruction_index BIGINT DEFAULT -1,
+    parent_instruction_program_id VARCHAR DEFAULT '',
+    top_instruction_program_id VARCHAR DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (slot, transaction_index, instruction_index)
+);
